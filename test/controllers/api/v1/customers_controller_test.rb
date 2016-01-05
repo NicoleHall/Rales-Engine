9075f705961c658ea1ce2d47a8cb68284ac46e58
Nicole Hall <nicolealexandrahall@gmail.com>
2016-01-04 21:16:55 -0700

    show action and index action are working for the api/v1/customers_controller. test pass

---
+++ test/controllers/api/v1/customers_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+class Api::V1::CustomersControllerTest < ActionController::TestCase
+
+  def setup
+    @customer = customers(:one)
+  end
+
+
+  test "should get customer index" do
+    get :index, format: :json
+    assert_response :success
+  end
+
+  test "should get customer show" do
+    get :show, format: :json, id: @customer.id
+    assert_response :success
+  end
+
+
+
+
+end
