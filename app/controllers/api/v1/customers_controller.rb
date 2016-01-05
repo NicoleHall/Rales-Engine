8a9a14d395441708e472538e3e7632a041df91dc
Nicole Hall <nicolealexandrahall@gmail.com>
2016-01-04 22:01:07 -0700

    working on getting customer create test to pass

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -10,4 +10,23 @@ class Api::V1::CustomersController < ApplicationController
     respond_with Customer.find_by(id: params[:id])
   end
 
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      respond_to do |format|
+        format.json {render json: @customer }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: @customer.errors.full_messages, status: 400}
+      end
+    end
+  end
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name)
+  end
+
 end
